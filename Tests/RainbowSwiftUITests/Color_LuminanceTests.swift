0b69a3cc69739ec2bbc5bdc8d4b932373b54c39f
Philip Niedertscheider <me@philprime.dev>
2024-02-22 14:25:48 +0100

    chore(ci): added testing job for iOS (#21)

--- Tests/RainbowSwiftUITests/Color_LuminanceTests.swift
+++ Tests/RainbowSwiftUITests/Color_LuminanceTests.swift
@@ -8,22 +8,22 @@ class Color_LuminanceTests: XCTestCase {
 
     func testLuminance_dynamicColor_shouldReturnLuminance() {
         // Act
-        let luminance = Color("luminance-test-color", bundle: Bundle.module)
+        let color = Color("luminance-test-color", bundle: Bundle.module)
         // Assert
-        XCTAssertEqual(luminance.luminance(), 0.704, accuracy: 0.01)
+        XCTAssertEqual(color.luminance(), 0.704, accuracy: 0.01)
     }
 
     func testLuminance_grayscaleColor_shouldReturnLuminance() {
         // Act
-        let luminance = Color(cgColor: CGColor(gray: 0.3, alpha: 1.0))
+        let color = Color(cgColor: CGColor(gray: 0.3, alpha: 1.0))
         // Assert
-        XCTAssertEqual(luminance.luminance(), 0.3, accuracy: 0.01)
+        XCTAssertEqual(color.luminance(), 0.3, accuracy: 0.01)
     }
 
     func testLuminance_rgbColor_shouldReturnLuminance() {
         // Act
-        let luminance = Color(.sRGB, red: 0.5, green: 0.8, blue: 0.2)
+        let color = Color(.sRGB, red: 0.5, green: 0.8, blue: 0.2)
         // Assert
-        XCTAssertEqual(luminance.luminance(), 0.6419, accuracy: 0.00001)
+        XCTAssertEqual(color.luminance(), 0.6419, accuracy: 0.00001)
     }
 }
